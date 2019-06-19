b29a153179ccf33346afb7be19eb7fa999dd01f1
Dowon Lee <edowon0623@gmail.com>
2019-06-20 00:02:20 +0900

    Upload spring boot project

--- src/main/java/com/example/post/controller/PostController.java
+++ src/main/java/com/example/post/controller/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
+@RequestMapping(value="/post")
 public class PostController {
 	@Autowired
 	private PostService guestBookService;
